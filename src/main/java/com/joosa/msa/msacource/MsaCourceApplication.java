e93cc85b2016d313bb0cec7f0dadee3e50521c79
JsooAh <joo1798@naver.com>
2024-08-30 00:14:57 +0900

    first commit

---
+++ src/main/java/com/joosa/msa/msacource/MsaCourceApplication.java
@@ -0,0 +1,13 @@
+package com.joosa.msa.msacource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaCourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joosa/msa/msacource/MsaCourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joosa.msa.msacource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaCourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
