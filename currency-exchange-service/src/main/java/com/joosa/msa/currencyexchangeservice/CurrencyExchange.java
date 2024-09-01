2f268266a4fff138b3f121db8a151a272fcf4f22
JsooAh <joo1798@naver.com>
2024-09-01 16:47:36 +0900

    240901 1647

---
+++ currency-exchange-service/src/main/java/com/joosa/msa/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,33 @@
+package com.joosa.msa.currencyexchangeservice;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "currency_exchange")
+public class CurrencyExchange {
+    @Id
+    private Long id;
+
+    @Column(name = "currency_from")
+    private String from;
+
+    @Column(name = "currency_to")
+    private String to;
+
+    private BigDecimal conversionMultiple;
+
+    private String environment;
+}
